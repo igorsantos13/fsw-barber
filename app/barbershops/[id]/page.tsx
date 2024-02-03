0bb12fa2079f9ae789ead4910c0ac828dcdda5c1
Igor Santos <igorsantos9513@outlook.com>
2024-02-03 18:28:34 -0300

    feat: add ServiceItem component & fix seed

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { db } from "@/app/_lib/prisma";
-import { LucideBadgeRussianRuble } from "lucide-react";
+import BarbershopInfo from "./_components/barbershop-info";
+import ServiceItem from "./_components/service-item";
 
 interface BarbershopDetailPageProps {
   params: {
@@ -11,12 +12,30 @@ interface BarbershopDetailPageProps {
 async function BarbershopDetailPage({ params }: BarbershopDetailPageProps) {
   //todo: redirecionar para homepage
   if (!params.id) return;
+
   const barbershop = await db.barbershop.findUnique({
     where: {
       id: params.id,
     },
+    include: {
+      services: true,
+    },
   });
-  return <div>{barbershop.name}</div>;
+  console.log(barbershop.services);
+
+  if (!barbershop) return;
+
+  return (
+    <div>
+      <BarbershopInfo barbershop={barbershop} />
+
+      <div className="px-5 flex flex-col gap-4 py-6">
+        {barbershop.services.map((service: any) => (
+          <ServiceItem key={service.id} service={service} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default BarbershopDetailPage;
